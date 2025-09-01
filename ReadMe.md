8700093079363b4a0d47e4e5a3a03d64232d13c7
Zaltu <swaugh@aigis.dev>
2025-09-01 08:56:11 -0400

    mild randomness improvement for fun

--- ReadMe.md
+++ ReadMe.md
@@ -2,3 +2,9 @@
 An incredibly basic image slideshow and categorizing app. Rating images via browser moves them to different folders.
 
 The whole thing is a garbage, hard-coded, undocumented, insecure mess. Don't look.
+
+The only semi-interesting performance note to make is that it's faster to randomize a full glob than cast a huge generator as a list and then randomize.  
+i.e.  
+`Path(random.choice(glob.glob("backend/dump/*"))).name`  
+is noticably faster than  
+`random.choice(list(Path("backend/dump").glob("*"))).name`
--- backend/app.py
+++ backend/app.py
@@ -9,13 +9,13 @@ from pathlib import Path
 import uvicorn
 from fastapi.middleware.cors import CORSMiddleware
 import os
+import random
+import glob
+
 
 abspathlocal = Path(os.path.dirname(os.path.abspath(__file__)))
-dumppath = "dump"
-
-
-print(abspathlocal)
-print((abspathlocal/dumppath).glob("*").__next__())
+dumppath = abspathlocal/"dump"
+globpath = str(dumppath)+"/*"
 
 bootleg_app = FastAPI()
 
@@ -28,24 +28,24 @@ bootleg_app.add_middleware(
 
 @bootleg_app.get("/filepath")
 async def get_image_name():
-    return (abspathlocal/dumppath).glob("*").__next__().name
+    return Path(random.choice(glob.glob(globpath))).name
 
 
 @bootleg_app.post("/judge")
 async def move_pic(request: Request):
     req = await request.json()
     if req["judgement"]=="yeet":
-        (abspathlocal / dumppath / req["path"]).rename(abspathlocal / "yeet" / req["path"])
+        (dumppath / req["path"]).rename(abspathlocal / "yeet" / req["path"])
     elif req["judgement"]=="yikes":
-        (abspathlocal / dumppath/ req["path"]).rename(abspathlocal / "yikes" / req["path"])
+        (dumppath/ req["path"]).rename(abspathlocal / "yikes" / req["path"])
     elif req["judgement"]=="mid":
-        (abspathlocal / dumppath/ req["path"]).rename(abspathlocal / "mid" / req["path"])
+        (dumppath/ req["path"]).rename(abspathlocal / "mid" / req["path"])
     elif req["judgement"]=="icanfixher":
-        (abspathlocal / dumppath/ req["path"]).rename(abspathlocal / "icanfixher" / req["path"])
+        (dumppath/ req["path"]).rename(abspathlocal / "icanfixher" / req["path"])
     elif req["judgement"]=="noice":
-        (abspathlocal / dumppath/ req["path"]).rename(abspathlocal / "noice" / req["path"])
+        (dumppath/ req["path"]).rename(abspathlocal / "noice" / req["path"])
     elif req["judgement"]=="poggers":
-        (abspathlocal / dumppath/ req["path"]).rename(abspathlocal / "poggers" / req["path"])
+        (dumppath/ req["path"]).rename(abspathlocal / "poggers" / req["path"])
 
 
 bootleg_app.mount(
